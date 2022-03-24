fa8d5ca0f668d82e45b9f2e76f0531674ae68248
Eric Rosário <ericsantos.r@hotmail.com>
2022-03-24 14:50:32 -0300

    Updated component to get handle errors

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,8 +1,11 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+
 const COLUMNS = [
     { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Annual Revenue', fieldName: REVENUE_FIELD.fieldApiName, type: 'currency' },
@@ -12,4 +15,13 @@ export default class AccountList extends LightningElement {
     columns = COLUMNS;
     @wire(getAccounts)
     accounts;
+
+    get errors(){
+        return (this.accounts.errors)?
+            reduceErrors(this.accounts.errors):[];
+    }
+
+
+
+
 }
